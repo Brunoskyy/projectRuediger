338f68f28b732b496a03d6bea11da695afd576ab
Artur Bruno Ferreira Duarte <dig.ArturBruno@opovo.local>
2019-12-13 09:57:11 -0300

    feat: btn collapse agora interage com o texto do parágrafo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,18 @@ btn.forEach( elem => {
     elem.addEventListener("click", function(e){
         e.preventDefault()
         // const target = `.${elem.parentNode.className}.chapter__paragraph`
-        const target = elem.parentNode.children[3]
-        setTimeout(function(){ target.style.opacity = '1' }, 100)
-        target.style.display = 'block'
         const btn1 = elem.parentNode.children[4]
-        btn1.style.display = 'none'
+        const target = elem.parentNode.children[3]
+        
+        if(btn1.textContent == 'Leia mais') {
+            setTimeout(function(){ target.style.opacity = '1' }, 100)
+            target.style.display = 'block'
+            btn1.textContent = 'Leia menos'
+        } else if(btn1.textContent == 'Leia menos') {
+            target.style.transition = '1000ms'
+            setTimeout(function(){ target.style.opacity = '0' }, 300)
+            target.style.display = 'none'
+            btn1.textContent = 'Leia mais'
+        }
     })
 })
